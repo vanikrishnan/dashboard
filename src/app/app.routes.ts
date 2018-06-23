b7bb4ace136a61359b36235a59ff5dc4c5014c6c
vanikrishnan <vanijeya1528@gmail.com>
2018-06-22 19:16:49 -0700

    interviewer dashboard ui completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,9 @@ export const routes: Routes = [
       path: '',
       loadChildren: 'app/registration/registration.module#RegistrationModule'
     },
+    {
+      path: 'interviewer',
+      loadChildren: 'app/interviewer/interviewer.module#InterviewerModule'
+    }
    
 ];
